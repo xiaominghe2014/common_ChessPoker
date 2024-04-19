f9a8604bb1f9466a9e9d20a6551dcd1b7afb3536
xiaoming <xiaominghe2014@gmail.com>
2024-04-19 14:09:30 +0800

    fix

--- build/tests/common.test.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var log_1 = require("../src/utils/log");
-var common = require("../src/utils/common");
-var range = common.range(10, 1);
-(0, log_1.log)("range(10,1):{}", range);
-//# sourceMappingURL=common.test.js.map
