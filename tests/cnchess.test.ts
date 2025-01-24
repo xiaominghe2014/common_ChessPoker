faa51702031d0c807921dc1bd590bce16a2fd5f7
xiaoming <xiaominghe2014@gmail.com>
2025-01-24 10:58:31 +0800

    fix

--- tests/cnchess.test.ts
+++ tests/cnchess.test.ts
@@ -1,7 +1,7 @@
 import CnChess from '../src/model/CnChess';
 
-let game = new CnChess.Game();
+let game = new CnChess.Game("rnbaka1nr/7c1/1c2b4/p1p1p1p1p/9/9/P1P1P1P1P/1C2B1NC1/4A4/RNBAK3R b");
 console.log(game.toString()); // 打印棋盘
 
-let mvs = game.getMoves(7,9);
+let mvs = game.getMoves(7,0);
 console.log(mvs); // 打印所有可走棋子
